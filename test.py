11f1e643428bdcbe1b3cbcb3f2f993e502a0b285
Al Miller <almiller@Als-MacBook-Pro.lan>
2023-03-28 13:59:11 -0400

    added args argument for better graphing

--- test.py
+++ test.py
@@ -12,9 +12,6 @@ class Trader:
     def __init__(self, stock='AAPL'):
         self.conf = Env("configuration.yaml").config
 
-        print(self.conf)
-        print("\n")
-
         self.url = f"https://yahoo-finance15.p.rapidapi.com/api/yahoo/hi/history/{stock}/15m"
         self.querystring = {"diffandsplits":"false"}
         self.headers = {
@@ -41,33 +38,37 @@ class Trader:
             stocks_arr.append(dic["items"][stock])
 
         df = pd.DataFrame(stocks_arr)
-        df.to_csv(file_name)
+        df.to_csv(file_name, index=True, index_label='index')
         return True 
 
     def get_from_csv(self, csv_file):
         return pd.read_csv(csv_file, parse_dates=True, index_col=0)
 
-    def plot(self, df):
+    def plot(self, df, *args):
         style.use('ggplot')
-        df['high'].plot()
+
+        for key in args:
+            plt.plot(df["date"], df[key[0]], key[1], label=key[0])
+        
+        plt.title("Stonks")
+        plt.xlabel('date')
+        plt.ylabel('price')
+        plt.legend()
         plt.show()
     
     def add_moving_average(self, df, size):
-        name = str(size) + "ma"
-        df[name] = df['close'].rolling(window=size, min_periods=0).mean()
+        ma_name = str(size) + "ma" # ex) 50ma
+        df[ma_name] = df['close'].rolling(window=size, min_periods=0).mean()
         df.dropna(inplace=True) # removes the entire row of all rows that have NaN
         return df
 
 
 
 trader = Trader('TSLA')
-df = trader.get_stocks()
-
-
-# trader.get_and_write_to_csv("tesla.csv")
-# df = trader.get_from_csv("tesla.csv")
-# trader.plot(df)
-# trader.add_moving_average(df, 50)
+# df = trader.get_stocks()
+# trader.get_and_write_to_csv("stock_csvs/tsla.csv")
+df = trader.get_from_csv("stock_csvs/apple.csv")
+trader.add_moving_average(df, 50)
+trader.plot(df, ('high', 'g'), ('low', 'r'), ('50ma', 'b'))
 
 print(df.head())
-print(df.tail())
