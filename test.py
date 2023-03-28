2d188d615625049718f2fc11d8cdc1b2150cdaeb
Al Miller <almiller@Als-MacBook-Pro.lan>
2023-03-28 14:59:22 -0400

    new plot volume function

--- test.py
+++ test.py
@@ -1,74 +1,27 @@
-import requests
-import json
-import pprint
+from datetime import datetime as dt
+from matplotlib import pyplot as plt, dates as mdates
 import pandas as pd 
-import matplotlib.pyplot as plt
-from matplotlib import style 
-import datetime as dt 
-from parse_conf import Env
 
 
-class Trader: 
-    def __init__(self, stock='AAPL'):
-        self.conf = Env("configuration.yaml").config
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
 
-        self.url = f"https://yahoo-finance15.p.rapidapi.com/api/yahoo/hi/history/{stock}/15m"
-        self.querystring = {"diffandsplits":"false"}
-        self.headers = {
-            "X-RapidAPI-Key": self.conf['api_key'],
-            "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
-        }
-
-    def get_stocks(self):
-        response = requests.request("GET", self.url, headers=self.headers, params=self.querystring)
-        dic = json.loads(response.text)
-
-        stocks_arr = []
-        for stock in dic["items"]:
-            stocks_arr.append(dic["items"][stock])
-
-        return pd.DataFrame(stocks_arr)
-
-    def get_and_write_to_csv(self, file_name):
-        response = requests.request("GET", self.url, headers=self.headers, params=self.querystring)
-        dic = json.loads(response.text)
-
-        stocks_arr = []
-        for stock in dic["items"]:
-            stocks_arr.append(dic["items"][stock])
-
-        df = pd.DataFrame(stocks_arr)
-        df.to_csv(file_name, index=True, index_label='index')
-        return True 
-
-    def get_from_csv(self, csv_file):
-        return pd.read_csv(csv_file, parse_dates=True, index_col=0)
-
-    def plot(self, df, *args):
-        style.use('ggplot')
-
-        for key in args:
-            plt.plot(df["date"], df[key[0]], key[1], label=key[0])
-        
-        plt.title("Stonks")
-        plt.xlabel('date')
-        plt.ylabel('price')
-        plt.legend()
-        plt.show()
-    
-    def add_moving_average(self, df, size):
-        ma_name = str(size) + "ma" # ex) 50ma
-        df[ma_name] = df['close'].rolling(window=size, min_periods=0).mean()
-        df.dropna(inplace=True) # removes the entire row of all rows that have NaN
-        return df
+dates = ["01/02/2020", "01/03/2020", "01/04/2020"]
+x_values = [dt.strptime(d, "%m/%d/%Y").date() for d in dates]
+y_values = [1, 2, 3]
 
 
-
-trader = Trader('TSLA')
-# df = trader.get_stocks()
-# trader.get_and_write_to_csv("stock_csvs/tsla.csv")
-df = trader.get_from_csv("stock_csvs/apple.csv")
-trader.add_moving_average(df, 50)
-trader.plot(df, ('high', 'g'), ('low', 'r'), ('50ma', 'b'))
+df = pd.DataFrame({'dates': x_values, "y_vals": y_values})
 
 print(df.head())
+
+ax = plt.gca()
+
+formatter = mdates.DateFormatter("%Y-%m-%d")
+ax.xaxis.set_major_formatter(formatter)
+locator = mdates.DayLocator()
+ax.xaxis.set_major_locator(locator)
+plt.plot(df['dates'], df['y_vals'])
+# plt.plot(x_values, y_values)
+
+plt.show()
---
+++ trader.py
@@ -0,0 +1,89 @@
+import requests
+import json
+import pprint
+import pandas as pd 
+import matplotlib.pyplot as plt
+from matplotlib import style 
+import datetime as dt 
+from parse_conf import Env
+from datetime import datetime as dt
+from matplotlib import dates as mdates
+
+
+class Trader: 
+    def __init__(self, stock='AAPL'):
+        self.conf = Env("configuration.yaml").config
+        self.stock = stock
+        self.url = self.conf['stock_url'] + self.stock
+        self.querystring = {"diffandsplits":"false"}
+        self.headers = {
+            "X-RapidAPI-Key": self.conf['api_key'],
+            "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
+        }
+
+    def get_stocks(self, time_interval):
+        if time_interval not in  ["5m", "15m", "30m", "1h", "1d", "1wk", "1mo", "3mo"]:
+            return False
+        
+        url = self.url + "/" + time_interval
+        response = requests.request("GET", url, headers=self.headers, params=self.querystring)
+        dic = json.loads(response.text)
+
+        stocks_arr = []
+        for stock in dic["items"]:
+            stocks_arr.append(dic["items"][stock])
+
+        return pd.DataFrame(stocks_arr)
+
+    def get_and_write_to_csv(self, time_interval):
+        file_name = "stock_csvs/" + self.stock + "_" +time_interval + ".csv"
+        df = self.get_stocks(time_interval)
+        df.to_csv(file_name, index=True, index_label='index')
+        return True 
+
+    def get_from_csv(self, csv_file):
+        return pd.read_csv(csv_file, parse_dates=True, index_col=0)
+
+    def plot(self, df, *args):
+        style.use('ggplot')
+
+        for key in args:
+            plt.plot(df["date"], df[key[0]], key[1], label=key[0])
+        
+        plt.title("Stonks")
+        plt.xlabel('date')
+        plt.ylabel('price')
+        plt.legend()
+        plt.show()
+
+    def plot_volume(self, df, *args):
+        style.use('ggplot')
+
+        ax1 =  plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+        ax2 =  plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+
+        ax1.plot(df['date'], df['high'], 'g')
+        ax1.plot(df['date'], df['low'], 'r')
+        ax1.plot(df['date'], df['50ma'], 'b')
+        ax2.bar(df['date'], df['volume'])
+
+        plt.show()
+    
+    def add_moving_average(self, df, size):
+        ma_name = str(size) + "ma" # ex) 50ma
+        df[ma_name] = df['close'].rolling(window=size, min_periods=0).mean()
+        df.dropna(inplace=True) # removes the entire row of all rows that have NaN
+        return df
+
+
+
+trader = Trader('BABA')
+# df = trader.get_stocks('1wk')
+# trader.get_and_write_to_csv("1d")
+df = trader.get_from_csv("stock_csvs/BABA_1d.csv")
+trader.add_moving_average(df, 50)
+# trader.plot_(df, ('high', 'g'), ('low', 'r'), ('50ma', 'b'))
+trader.plot_volume(df)
+# trader.plot(df, ('high', 'g'))
+
+print(df.head())
