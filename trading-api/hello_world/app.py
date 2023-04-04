fa2f3bff255bab2001944c03ffad57b46f30d37f
Al Miller <almiller@Als-MacBook-Pro.lan>
2023-04-04 19:25:59 -0400

    api POC working

--- trading-api/hello_world/app.py
+++ trading-api/hello_world/app.py
@@ -1,7 +1,25 @@
 import json
+from urllib import response
+import boto3
+import logging 
+import requests
+from decimal import Decimal 
 
-# import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
+dynamodbTableName = 'paper-trader-transactions'
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(dynamodbTableName)
+
+getMethod = 'GET'
+postMethod = 'POST'
+patchMethod = 'PATCH'
+deleteMethod = 'DELETE'
+
+healthPath = '/health'
+productPath = '/product'
+productsPath = '/products'
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -25,18 +43,169 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
+    logger.info("## EVENT:")
+    logger.info(event)
+    httpMethod = event['httpMethod']
+    logger.info("## METHOD: ")
+    logger.info(httpMethod)
+    
+    path = event['path']
+    logger.info("## path: ")
+    logger.info(path)
+    
+    body = event['body']
+    logger.info("## body: ")
+    logger.info(body)
+    logger.info("type of body: ")
+    logger.info(type(body))
 
-    #     raise e
+    if httpMethod == getMethod and path == healthPath:
+        response = buildResponse(200)
+    elif httpMethod == getMethod and path == productPath:
+        response = getProduct(event['queryStringParameters']['productId'])
+    elif httpMethod == getMethod and path == productsPath: 
+        response = getProducts()
+    elif httpMethod == postMethod and path == productPath: 
+        logger.info("inside save product")
+        response = saveProduct(event['body'])
+    elif httpMethod == patchMethod and path == productPath: 
+        requestBody = event['body']
+        response = modifyProduct(requestBody['productId'], requestBody['updateKey'], requestBody['updateValue'])
+    elif httpMethod == deleteMethod and path == productPath: 
+        requestBody = json.loads(event['body'])
+        response = deleteProduct(requestBody['productId'])
+    else: 
+        # response = buildResponse(404, "Not Found")
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "hello world with change!",
+                # "location": ip.text.replace("\n", "")
+            }),
+        }
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+    return response
+
+
+def getProduct(productId): 
+    try: 
+        response = table.get_item(
+            Key={
+                'productId': productId
+            }
+        )
+        if 'Item' in response: 
+            return buildResponse(200, response['Item'])
+        else: 
+            return buildResponse(404, {'Message': 'ProductId: %s not found' % productId})
+    except: 
+        logger.exception("something went wrong in the getProduct() method")
+
+
+
+def getProducts():
+    try: 
+        response = table.scan()
+        result = response['Items']
+
+        while 'LastEvaluatedKey' in response: 
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            result.extend(response['Items'])
+        
+        body = {
+            'products': response
+        }
+        return buildResponse(200, body)
+    except: 
+        logger.exception("something went wrong in getProducts() method")
+
+
+
+def saveProduct(requestBody): 
+    try: 
+        logger.info("starting saveProduct() function")
+
+        if isinstance(requestBody, dict): 
+            pass
+        if isinstance(requestBody, str): 
+            requestBody = json.loads(requestBody)
+
+        logger.info(type(requestBody))
+        table.put_item(Item=requestBody)
+        
+        body = {
+            'Operation': 'SAVE', 
+            'Message': 'SUCCESS', 
+            'Item': requestBody
+        }
+        return buildResponse(200, body)
+    except: 
+        logger.exception("something went wrong in saveProduct() method")
+
+
+def modifyProduct(productId, updateKey, updateValue): 
+    try: 
+        response = table.update_item(
+            Key={
+                'productId': productId
+            },
+            UpdateExpression='set %s = :value' % updateKey, 
+            ExpressionAttributeValues={
+                ':value': updateValue
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        body = {
+            'Operation': 'UPDATE',
+            'Message': 'SUCCESS',
+            'UpdatedAttrebutes': response
+        }
+        return buildResponse(200, body)
+    except: 
+        logger.exception("something went wrong in modifyProduct() method")
+
+
+def deleteProduct(productId):
+    try: 
+        response = table.delete_item(
+            Key={
+                'productId': productId
+            },
+            ReturnValues='ALL_OLD'
+        )
+        body = {
+            'Operation': 'DELETE',
+            'Message': 'SUCCESS',
+            'deletedItem': response
+        }
+        return buildResponse(200, body)
+    except: 
+        logger.exception("something went wrong in deleteProduct() method")
+
+
+def buildResponse(statusCode, body=None): 
+    logger.info("MADE IT TO buildRespose FUNCTION")
+    response = {
+        'statusCode': statusCode, 
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,DELETE',
+            'Access-Control-Allow-Credentials': True,
+        }
     }
+    if body is not None: 
+        response['body'] = json.dumps(body, cls=CustomEncoder)
+    logger.info("## RESPONSE ")
+    logger.info(response)
+    return response
+
+
+# object we get from dynamo is in decimal 
+# decimal is not supported by json encoder 
+class CustomEncoder(json.JSONEncoder): 
+    def default(self, obj): 
+        if isinstance(obj, Decimal): 
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
---
+++ trading-api/test.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+shit = {'resource': '/hello', 'path': '/hello/', 'httpMethod': 'GET', 'headers': {'Accept': '*/*', 'CloudFront-Forwarded-Proto': 'https', 'CloudFront-Is-Desktop-Viewer': 'true', 'CloudFront-Is-Mobile-Viewer': 'false', 'CloudFront-Is-SmartTV-Viewer': 'false', 'CloudFront-Is-Tablet-Viewer': 'false', 'CloudFront-Viewer-ASN': '12271', 'CloudFront-Viewer-Country': 'US', 'Host': 'rbks09r44k.execute-api.us-east-1.amazonaws.com', 'User-Agent': 'curl/7.79.1', 'Via': '2.0 cd958e502c6aea704f0f824e60431e72.cloudfront.net (CloudFront)', 'X-Amz-Cf-Id': 'PwDwr6B3VdDOLXPZoWVdC6qjo-ap0wLqh1AQyg4FFE-XW4xHBtT8iA==', 'X-Amzn-Trace-Id': 'Root=1-642c9cdf-002259364e64fa8215769a4f', 'X-Forwarded-For': '47.230.198.14, 15.158.35.207', 'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https'}, 'multiValueHeaders': {'Accept': ['*/*'], 'CloudFront-Forwarded-Proto': ['https'], 'CloudFront-Is-Desktop-Viewer': ['true'], 'CloudFront-Is-Mobile-Viewer': ['false'], 'CloudFront-Is-SmartTV-Viewer': ['false'], 'CloudFront-Is-Tablet-Viewer': ['false'], 'CloudFront-Viewer-ASN': ['12271'], 'CloudFront-Viewer-Country': ['US'], 'Host': ['rbks09r44k.execute-api.us-east-1.amazonaws.com'], 'User-Agent': ['curl/7.79.1'], 'Via': ['2.0 cd958e502c6aea704f0f824e60431e72.cloudfront.net (CloudFront)'], 'X-Amz-Cf-Id': ['PwDwr6B3VdDOLXPZoWVdC6qjo-ap0wLqh1AQyg4FFE-XW4xHBtT8iA=='], 'X-Amzn-Trace-Id': ['Root=1-642c9cdf-002259364e64fa8215769a4f'], 'X-Forwarded-For': ['47.230.198.14, 15.158.35.207'], 'X-Forwarded-Port': ['443'], 'X-Forwarded-Proto': ['https']}, 'queryStringParameters': None, 'multiValueQueryStringParameters': None, 'pathParameters': None, 'stageVariables': None, 'requestContext': {'resourceId': 'x6qouf', 'resourcePath': '/hello', 'httpMethod': 'GET', 'extendedRequestId': 'C31y_EIgIAMFnWw=', 'requestTime': '04/Apr/2023:21:55:43 +0000', 'path': '/Prod/hello/', 'accountId': '719501685385', 'protocol': 'HTTP/1.1', 'stage': 'Prod', 'domainPrefix': 'rbks09r44k', 'requestTimeEpoch': 1680645343655, 'requestId': 'ae4e20be-00a7-4df6-b5df-04f95e48bbdb', 'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None, 'caller': None, 'sourceIp': '47.230.198.14', 'principalOrgId': None, 'accessKey': None, 'cognitoAuthenticationType': None, 'cognitoAuthenticationProvider': None, 'userArn': None, 'userAgent': 'curl/7.79.1', 'user': None}, 'domainName': 'rbks09r44k.execute-api.us-east-1.amazonaws.com', 'apiId': 'rbks09r44k'}, 'body': {"productId": "001", "name": "lucky_charms"}, 'isBase64Encoded': False}
+
+# print(json.dumps(shit))
+
+url = "https://bjnhhj5gd2.execute-api.us-east-1.amazonaws.com/Prod"
+say_hi = url + '/hello'
+get_url = url + '/products'
+post_url = url + '/product'
+
+myobj = {"productId": "003", "name": "poop"}
+# print(type(shit))
+# print(isinstance(shit, dict))
+
+# x = requests.get(say_hi)
+x = requests.get(get_url)
+# x = requests.post(post_url, json = myobj)
+
+print(x.text)
