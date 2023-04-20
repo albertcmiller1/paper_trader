f982f4d1c506ff40eaaa2fe2c48939a737d69fe5
Albert Miller <49928434+albertcmiller1@users.noreply.github.com>
2023-04-20 18:01:41 -0400

    Update load_test_data.py

--- admin/load_test_data.py
+++ admin/load_test_data.py
@@ -20,7 +20,6 @@ for item in payload:
     response = requests.post(post_url, json = item) # this works when data is all strings
     # response = requests.post(post_url, json = json.loads(item)) # this does not work
     print("\n")
-    print("respone: ")
     print(response.text)
 
 
